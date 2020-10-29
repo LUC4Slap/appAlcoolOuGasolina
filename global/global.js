cbf3a0f158f9816cc97946d680227c609f7a70e1
LUC4Slap <lucaslap27@gmail.com>
2020-10-29 09:48:03 -0400

    Adiciona splash screen e icone

--- global/global.js
+++ global/global.js
@@ -1,6 +1,9 @@
 const global = {
     urlMarcasCarro: "https://parallelum.com.br/fipe/api/v1/carros/marcas",
     urlMarcasMoto: "https://parallelum.com.br/fipe/api/v1/motos/marcas",
-    urlConsultaCnpj: "https://www.receitaws.com.br/v1/cnpj/"
+    urlConsultaCnpj: "https://www.receitaws.com.br/v1/cnpj/",
+    urlEncurtarUrl: "https://url-shortener-service.p.rapidapi.com/shorten",
+    //colocar nos headers da requisição X-RapidAPI-Key: 7ef0023ea0mshe5462d94f5d7854p188505jsn9267404dc9cd
+    
 }
 export default global;
